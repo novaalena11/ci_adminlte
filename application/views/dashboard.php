be0b79a3b3de2ddbc7621c8c8286b991e64ba9a9
novaalena11 <novaalena42@gmail.com>
2025-05-15 20:20:41 +0700

    Initial commit

---
+++ application/views/dashboard.php
@@ -0,0 +1,18 @@
+<?php $this->load->view('templates/header'); ?>
+<?php $this->load->view('templates/sidebar'); ?>
+
+<div class="content-wrapper">
+  <section class="content-header">
+    <div class="container-fluid">
+      <h1>Halaman Dashboard</h1>
+    </div>
+  </section>
+
+  <section class="content">
+    <div class="container-fluid">
+      <p>Selamat datang di dashboard!</p>
+    </div>
+  </section>
+</div>
+
+<?php $this->load->view('templates/footer'); ?>
---
+++ application/views/templates/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Dashboard</title>
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/plugins/fontawesome-free/css/all.min.css'); ?>">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/plugins/bootstrap/css/bootstrap.min.css'); ?>">
+  <!-- AdminLTE -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/adminlte.min.css'); ?>">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
