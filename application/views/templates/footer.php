5fe0bbaaeb6bb48a4987f9abf40ea4418b440acd
novaalena11 <novaalena42@gmail.com>
2025-05-15 23:04:29 +0700

    Integrasi AdminLTE dengan CodeIgniter 3

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,8 +1,5 @@
-<!-- jQuery -->
-<script src="<?php echo base_url('assets/plugins/jquery/jquery.min.js'); ?>"></script>
-<!-- Bootstrap 4 -->
-<script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
-<!-- AdminLTE App -->
-<script src="<?php echo base_url('assets/dist/js/adminlte.min.js'); ?>"></script>
+</div>
+<script src="<?php echo base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/adminlte.min.js'); ?>"></script>
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,14 +2,10 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Dashboard</title>
-
-  <!-- Font Awesome -->
+  <title>AdminLTE</title>
   <link rel="stylesheet" href="<?php echo base_url('assets/plugins/fontawesome-free/css/all.min.css'); ?>">
-  <!-- Bootstrap -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/plugins/bootstrap/css/bootstrap.min.css'); ?>">
-  <!-- AdminLTE -->
-  <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/adminlte.min.css'); ?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/adminlte.min.css'); ?>">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
